e23ca78a9c9c32d2c411666f2581956133fe62f3
ted <mairurat@gmail.com>
2023-01-23 13:31:03 +0300

    Update

--- src/Views/ProjectCard.tsx
+++ src/Views/ProjectCard.tsx
@@ -1,36 +1,41 @@
-import { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./css/card.css";
-// import lalo from "../assets/lalo.png";
 import TeamMember from "./Components/TeamMember";
 import { Link } from "react-router-dom";
-// import axios from "axios";
 import { ProjectContext } from "./Context";
-// import { Carousel } from "react-responsive-carousel";
+import { Carousel } from "react-responsive-carousel";
 
 function ProjectCard() {
   const { projects } = useContext(ProjectContext);
 
   return (
     <>
-      <div className="card-main">
-        <div className="more">
-          <div className="btn-container">
-            {projects.map((project: any) => (
-              <div key={project.id}>
-                <Link to={`project/?projectId=${project.proj_name}`}>
-                  <p>{project.proj_name}</p>
-                </Link>
-              </div>
-            ))}
-            <button>Lines of code</button>
-            <button>live project</button>
-          </div>
-        </div>
-      </div>
-
+      <Carousel className="project-cards" showStatus={false}>
+        {projects.map((project: any) => (
+          <>
+            <div className="card-main">
+              <Link to="project">
+                <div className="more">
+                  <div key={project.id}>
+                    <Link to={`project/?projectId=${project.proj_name}`}>
+                      <p>{project.proj_name}</p>
+                      <div className="btn-container">
+                        <button>Lines of code</button>
+                        <button>
+                          <Link to="">live project</Link>
+                        </button>
+                      </div>
+                    </Link>
+                  </div>
+                </div>
+              </Link>
+            </div>
+            <TeamMember />
+          </>
+        ))}
+      </Carousel>
       <br />
       <br />
-      <TeamMember />
     </>
   );
 }
