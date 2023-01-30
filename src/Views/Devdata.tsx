9791ccca01535535b7b894fecca6423c9dcaae89
brian <mairurat@gmail.com>
2023-01-30 20:44:36 +0300

    Update dev data

--- src/Views/Devdata.tsx
+++ src/Views/Devdata.tsx
@@ -1,32 +1,49 @@
 import "./css/devdata.css";
-import dev from "../assets/sample.png";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Devdata = () => {
+  const [devs, setDevs] = useState([]);
+
+  //Function to get all developers at NGeni Labs
+  const devData = async () => {
+    const devs: any = await axios.get("http://localhost:8000/index/devs");
+    // console.log("Developers:", devs.data);
+    setDevs(devs.data);
+  };
+
+  console.log("Names:", devs);
+
+  useEffect(() => {
+    devData();
+  });
+
   return (
-    <div className="devdata_container">
-      <div className="devdata_details">
-        <div className="devdata_image">
-          <img src={dev} />
-        </div>
-        <div className="devdata_name">
-          <h3>
-            Dauglous Omambia&nbsp;<span>(Omambia.pyther)</span>
-          </h3>
-          <p>Backend Developer</p>
-        </div>
-      </div>
-      <div className="devdata_details">
-        <div className="devdata_image">
-          <img src={dev} />
-        </div>
-        <div className="devdata_name">
-          <h3>
-            Dauglous Omambia&nbsp;<span>(Omambia.pyther)</span>
-          </h3>
-          <p>Backend Developer</p>
-        </div>
-      </div>
-    </div>
+    <>
+      {devs.map((dev: any) => {
+        let name = dev.name;
+        let shortName = dev.short_name;
+        let pic = dev.profile_img_link;
+        let gitLink = dev.profile_link;
+
+        return (
+          <div className="devdata_container">
+            <div className="devdata_details">
+              <div className="devdata_image">
+                <img src={pic} />
+              </div>
+              <div className="devdata_name">
+                <h3>
+                  {name}&nbsp;<span>({shortName})</span>
+                </h3>
+                <p>Backend Developer</p>
+                <i>{gitLink}</i>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 };
 
