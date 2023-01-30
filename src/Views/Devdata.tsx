046ea9ecf3cd820c46bb426ccfeb7cfea2382096
brian <mairurat@gmail.com>
2023-01-30 19:43:23 +0300

    Dev page buildup

--- src/Views/Devdata.tsx
+++ src/Views/Devdata.tsx
@@ -1,8 +1,33 @@
-import React from "react";
 import "./css/devdata.css";
+import dev from "../assets/sample.png";
 
 const Devdata = () => {
-  return <div>Devdata</div>;
+  return (
+    <div className="devdata_container">
+      <div className="devdata_details">
+        <div className="devdata_image">
+          <img src={dev} />
+        </div>
+        <div className="devdata_name">
+          <h3>
+            Dauglous Omambia&nbsp;<span>(Omambia.pyther)</span>
+          </h3>
+          <p>Backend Developer</p>
+        </div>
+      </div>
+      <div className="devdata_details">
+        <div className="devdata_image">
+          <img src={dev} />
+        </div>
+        <div className="devdata_name">
+          <h3>
+            Dauglous Omambia&nbsp;<span>(Omambia.pyther)</span>
+          </h3>
+          <p>Backend Developer</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Devdata;
