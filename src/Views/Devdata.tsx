1898c084b305a44f8ddd9319dc68c88c37b819de
brian <mairurat@gmail.com>
2023-01-31 17:03:39 +0300

    Header in developers page

--- src/Views/Devdata.tsx
+++ src/Views/Devdata.tsx
@@ -1,12 +1,19 @@
 import "./css/devdata.css";
+import { useContext } from "react";
 import axios from "axios";
+import { ProjectContext } from "./Context";
 import { useEffect, useState } from "react";
 import { Config } from "../config/config";
 import HashLoader from "react-spinners/HashLoader";
+import { TypeAnimation } from "react-type-animation";
+import { NavLink } from "react-router-dom";
+import Title from "../assets/title.png";
 
 const Devdata = () => {
+  const { projects } = useContext(ProjectContext);
   const [devs, setDevs] = useState([]);
   const [loader, setLoader] = useState(true);
+  const [getStacks, setGetStacks] = useState([]);
   let url = Config.URL;
 
   //Function to get all developers at NGeni Labs
@@ -16,7 +23,15 @@ const Devdata = () => {
     setDevs(devs.data);
   };
 
-  console.log("Names:", devs);
+  //Function to get all stacks from all projects
+  const getStack = async () => {
+    const stack = await axios.get(`${url}/index/tags`);
+    console.log("Stack Listed:", stack.data);
+    let stacks = stack.data;
+    setGetStacks(stacks);
+  };
+
+  console.log("Received Projects:", projects);
 
   useEffect(() => {
     devData();
@@ -28,6 +43,50 @@ const Devdata = () => {
 
   return (
     <>
+      <>
+        <div>
+          <div className="header">
+            <img src={Title} alt="title describe" />
+            <TypeAnimation
+              sequence={["Team and Skill Matching Engine", 1000, () => {}]}
+              wrapper="div"
+              cursor={true}
+              repeat={Infinity}
+              style={{ fontSize: "1.5em", color: "#fff" }}
+            />
+          </div>
+          <div className="header_data">
+            <NavLink className="devdata_link" to="/">
+              <p>Projects</p>
+            </NavLink>
+            <NavLink to="devdata" className="devdata_link">
+              <p>Developers</p>
+            </NavLink>
+          </div>
+        </div>
+        {/* <div className="search-bar">
+          <form>
+            <select>
+              <option>Choose Stack</option>
+              {getStacks.map((item: any, index) => (
+                <option key={index} value={item.name}>
+                  {item.name}
+                </option>
+              ))}
+            </select>
+          </form> */}
+        {/* <div className="current-tags">
+            {tags.map((item, i) => (
+              <div key={i + 1} className="tag">
+                <p>{item}</p>
+              </div>
+            ))}
+          </div> */}
+        {/* <i className="project_number">
+            Found&nbsp;{projects.length}&nbsp;projects for stack&nbsp;{tags}
+          </i> */}
+        {/* </div> */}
+      </>
       {loader ? (
         <div className="item">
           <HashLoader
