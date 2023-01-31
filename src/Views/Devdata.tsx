4043c9aaf0036b320759c23fa2b4de59480d5f3b
brian <mairurat@gmail.com>
2023-01-31 19:05:26 +0300

    Update on dev profile page

--- src/Views/Devdata.tsx
+++ src/Views/Devdata.tsx
@@ -1,19 +1,21 @@
 import "./css/devdata.css";
-import { useContext } from "react";
+import Title from "../assets/title.png";
 import axios from "axios";
-import { ProjectContext } from "./Context";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { Config } from "../config/config";
 import HashLoader from "react-spinners/HashLoader";
 import { TypeAnimation } from "react-type-animation";
 import { NavLink } from "react-router-dom";
-import Title from "../assets/title.png";
+import { ProjectContext, IProjects } from "./Context";
 
 const Devdata = () => {
-  const { projects } = useContext(ProjectContext);
+  const [tags, setTags] = useState<Array<string>>([]);
+  // const { projects } = useContext(ProjectContext);
+  const [projects, setProjects] = useState<Array<IProjects>>([]);
   const [devs, setDevs] = useState([]);
-  const [loader, setLoader] = useState(true);
   const [getStacks, setGetStacks] = useState([]);
+  const [showPage, setShowPage] = useState(false);
+  const [loader, setLoader] = useState(true);
   let url = Config.URL;
 
   //Function to get all developers at NGeni Labs
@@ -21,6 +23,34 @@ const Devdata = () => {
     const devs: any = await axios.get(`${url}/index/devs`);
     // console.log("Developers:", devs.data);
     setDevs(devs.data);
+  };
+
+  //Function to handle selected stack to be called
+  const handleStack = (e: any) => {
+    e.preventDefault();
+    let tagList = [...tags, e.target.value];
+    if (!tags.includes(e.target.value)) {
+      setTags(tagList);
+      let newList: string = tagList.join();
+      // console.log("NewList", newList);
+      getData(newList);
+      // console.log("Tags:", tagList);
+    }
+  };
+
+  //Function to submit choosen stack and find resp projects
+  const getData = async (stackToSearch: string) => {
+    setShowPage(false);
+    const endpoint: string = `${url}/index/projects/tags/${stackToSearch}`;
+    try {
+      const { data } = await axios.get(endpoint);
+      setProjects([...data]);
+      // console.log("Projects", data);
+      // console.log("Project Length:", data.length);
+      setShowPage(true);
+    } catch (error: any) {
+      console.error("Error:", error.message);
+    }
   };
 
   //Function to get all stacks from all projects
@@ -31,11 +61,15 @@ const Devdata = () => {
     setGetStacks(stacks);
   };
 
-  // console.log("Received Projects:", projects);
+  console.log("Names:", devs);
 
   useEffect(() => {
     devData();
   });
+
+  useEffect(() => {
+    getStack();
+  }, []);
 
   useEffect(() => {
     setTimeout(() => setLoader(false), 2000);
@@ -64,9 +98,13 @@ const Devdata = () => {
             </NavLink>
           </div>
         </div>
-        {/* <div className="search-bar">
+        <div className="search-bar">
           <form>
-            <select>
+            <select
+              onChange={(e) => {
+                handleStack(e);
+              }}
+            >
               <option>Choose Stack</option>
               {getStacks.map((item: any, index) => (
                 <option key={index} value={item.name}>
@@ -74,18 +112,18 @@ const Devdata = () => {
                 </option>
               ))}
             </select>
-          </form> */}
-        {/* <div className="current-tags">
+          </form>
+          <div className="current-tags">
             {tags.map((item, i) => (
               <div key={i + 1} className="tag">
                 <p>{item}</p>
               </div>
             ))}
-          </div> */}
-        {/* <i className="project_number">
+          </div>
+          <i className="project_number">
             Found&nbsp;{projects.length}&nbsp;projects for stack&nbsp;{tags}
-          </i> */}
-        {/* </div> */}
+          </i>
+        </div>
       </>
       {loader ? (
         <div className="item">
