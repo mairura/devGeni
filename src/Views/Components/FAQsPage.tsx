e132265faf4f6213147edc6f9911f779c5755bd0
brian <mairurat@gmail.com>
2023-02-24 20:04:11 +0300

    Updates

--- src/Views/Components/FAQsPage.tsx
+++ src/Views/Components/FAQsPage.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import "../css/about.css";
+import pic from "../../assets/FAQS.jpg";
 
 const FAQsPage = () => {
   return (
     <div className="faqsContainer">
-      <div>Faqs</div>
+      {/* <video muted loop poster={pic} height="300" width="400" controls>
+        <source src="../../assets/WhatsAppVideo.mp4" type="video/mp4" />
+      </video> */}
+      <div className="faqsHeader">Faqs</div>
       <div className="faqsBox">
         <p>Do you offer Web3.0 services?</p>
       </div>
