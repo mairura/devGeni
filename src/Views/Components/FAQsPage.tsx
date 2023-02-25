8d13c4e744093a281e7ee2b52019f75bfafaf863
brian <mairurat@gmail.com>
2023-02-25 14:00:45 +0300

    UPdates

--- src/Views/Components/FAQsPage.tsx
+++ src/Views/Components/FAQsPage.tsx
@@ -5,10 +5,11 @@ import pic from "../../assets/FAQS.jpg";
 const FAQsPage = () => {
   return (
     <div className="faqsContainer">
-      {/* <video muted loop poster={pic} height="300" width="400" controls>
-        <source src="../../assets/WhatsAppVideo.mp4" type="video/mp4" />
-      </video> */}
-      <div className="faqsHeader">Faqs</div>
+      <video src="../../assets/WhatsAppVideo.mp4" muted loop autoPlay />
+      <div className="faqsHeader">
+        {/* <img src={pic} />  */}
+        Faqs
+      </div>
       <div className="faqsBox">
         <p>Do you offer Web3.0 services?</p>
       </div>
