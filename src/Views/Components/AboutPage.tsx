dbb1ab335fbf79d1eb51495f1994fb3c7b95344f
brian <mairurat@gmail.com>
2023-02-27 15:27:46 +0300

    Updated content for about page

--- src/Views/Components/AboutPage.tsx
+++ src/Views/Components/AboutPage.tsx
@@ -10,6 +10,26 @@ const AboutPage = () => {
       <div className="aboutLogo">
         <img src={Logo} alt="logo" />{" "}
       </div>
+      <h4>About Us</h4>
+      <p>
+        DevGeni is a platform for exceptional developers specializing in
+        front-end to back-end, Web3, and blockchain technologies. Imagine an app
+        with a Project reference, a developer to talk to instantly, and quality
+        services. DevGeni provides all that. By browsing through DevGeni you can
+        choose any tech stack to get a glimpse of their skills, past projects,
+        and expertise. We also make your work easier by helping you choose the
+        products/tech stack you want to be used in your application. You can
+        talk directly with the developer and specify what you want developed.
+        After agreeing on the product development, we work closely with you
+        every step of the way to ensure the final product fits your needs
+        perfectly. Our team of experts uses the latest technologies and software
+        development methodologies to ensure the highest level of quality,
+        reliability, and security for your software. Our expertise, combined
+        with cutting-edge technology and a customer-first approach, enables us
+        to deliver outstanding results for our clients. We understand that every
+        business is unique, and that's why we offer custom software development
+        services that are tailored to meet your specific needs. 🔥
+      </p>
       <TypeAnimation
         sequence={["CODE 🙂 CODE 🖥", 5000, "CODE 💻 ", 5000]}
         wrapper="h5"
@@ -17,20 +37,7 @@ const AboutPage = () => {
         repeat={Infinity}
         style={{ fontSize: "1.5em" }}
       />
-      <p>
-        Next generation FULLSTACK WEB3 DEVELOPERS & TECH strategists building
-        ComPlex and cross-platform Applications, Bots & dApps for Blockchain,
-        Fintech, Cryptocurrencies & Markets Trading. We develop across the
-        Entire ECOSYSTEM & 💗 COMPLEXITY! Mobile Payments, API & DeFi bridges,
-        Digital wallets...? We got THIS. It's in OUR BLOOD 😍 We’re GENERATION
-        M-Pesa👨👨👦👦 moving money Digitally since 2007🔥
-      </p>
-      <i>
-        Building prominent applications, dApps, automation tools, plugins,
-        bridges/gateways, wallets & bots for the world's hottest Blockchain
-        Protocols, Institutional & Professional Markets Traders, FinTech
-        platforms, and Cryptocurrency projects.
-      </i>
+
       <div className="btn_about">
         <Link to="/contact">
           <button>Get Back</button>
